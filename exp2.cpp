93ea422a38db79abfea7b2fc6192a297367077f0
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-09-11 20:05:52 +0530

    exp2

--- exp2.cpp
+++ exp2.cpp
@@ -1,46 +1,25 @@
+//Call by value and Call by Reference
 #include<iostream>
 using namespace std;
-
-class Student{
-    public:
-    string name;
-    int RollNum;
-    int grade;
-    
-    void input()
-    {
-        cout<<"Enter Student's name: ";
-        getline(cin,name);
-        cout<<"Enter roll number: ";
-        cin>>RollNum;
-        cout<<"Enter grade of the student : ";
-        cin>>grade;
-    }
-    void display()
-    {
-        cout<<"Name of student: "<<name<<endl;
-        cout<<"Roll number: "<<RollNum<<endl;
-        cout<<"Grade of the student: "<<grade<<endl;
-    }
-    void displayGrade(int marks)
-    {
-       if(marks>=60)
-       cout<<"1st Division";
-       else if(marks>=50 && marks<60)
-       cout<<"2nd Division";
-       else if(marks>=33 && marks<50)
-       cout<<"3rd Division";
-       else if(marks<=33)
-       cout<<"Fail";
-    }
-};
-int main()
-{
-    Student s;
-    s.input();
-    s.display();
-    int marks;
-    cin>>marks;
-    s.displayGrade(marks);
-    return 0;
+void data1(int x){
+    x=50;
+    cout<<x;
 }
+void data2(int &x){
+    x=50;
+    cout<<x;
+}
+
+int main(){
+    int m=100;
+    cout<<"Call by value : ";
+    data1(m);
+    cout<<" "<<m<<" #Value not changed.";
+    cout<<endl;
+    cout<<"Call by Reference : ";
+    data2(m);
+    cout<<" "<<m<<" #Value has been changed.";
+    cout<<endl; 
+}
+
+
