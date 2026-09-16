ac8914ec851923ed83506a04363e0894a57e9327
devanshiteotia162 <teotia.devanshi2102@gmail.com>
2026-09-16 10:17:11 +0530

    ques 3

---
+++ ques3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Patient {
+private:
+    int patientId;
+    string patientName;
+
+    static int activePatients;
+
+public:
+    Patient(int id, string name) {
+        patientId = id;
+        patientName = name;
+        activePatients++;
+    }
+
+    ~Patient() {
+        activePatients--;
+    }
+
+    static void showActivePatients() {
+        cout << "Active Patients: " << activePatients << endl;
+    }
+};
+
+int Patient::activePatients = 0;
+
+int main() {
+
+    Patient p1(101, "Aman");
+    Patient p2(102, "Riya");
+
+    Patient::showActivePatients();
+
+    {
+        Patient p3(103, "Rahul");
+        Patient::showActivePatients();
+    }  
+
+    Patient::showActivePatients();
+
+    return 0;
+}
